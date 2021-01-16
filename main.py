d89df86b8794d1f20ba5efe0795539bdd06ef0a3
kanishk779 <kashyapamankumar54@gmail.com>
2021-01-16 13:33:31 +0530

    Add joinTable method

--- main.py
+++ main.py
@@ -8,52 +8,62 @@ from collections import OrderedDict
 
 class MiniSQL:
     def __init__(self):
-        super().__init__()
         self.tableInfo = OrderedDict()
         self.database = OrderedDict() # database[TABLE_NAME][COLUMN_NAME] -> gives list of values in this column
         self.getMetaInfo()
         self.fillContent()
     
     def getMetaInfo(self):
+        """
+        Reads the metadata.txt file to read in the schema of the database.
+        Each table in database is stored in 
+        """
         try:
             infoFile = open('metadata.txt', 'r')
         except FileNotFoundError as fn:
             print("metadata.txt not found")
-            raise
+            raise fn
         else:
             tableStarted = False
             tableName = ""
-            for row in infoFile:
-                if row.strip() == '<begin_table>':
+            for ro in infoFile:
+                if ro.strip() == '<begin_table>':
                     tableStarted = True
                     continue
-                if row.strip() == '<end_table':
+                if ro.strip() == '<end_table>':
                     continue
                 if tableStarted:
                     tableStarted = False
-                    tableName = row.strip()
+                    tableName = ro.strip()
                     self.tableInfo[tableName] = []
                     continue
                 # append the column names into the table dict
-                self.tableInfo[tableName].append(row.strip())
+                self.tableInfo[tableName].append(ro.strip())
     
     @staticmethod
     def getCSV(name):
         """
         Provides the content of the CSV file
         """
-        content = []
         try:
             tabFile = open(name, 'r')
         except Exception as e:
             print(name + " file does not exist")
             raise FileExistsError
         else:
+            content = []
+            first = True # we do not have to read the first line of csv file
             for row in tabFile:
+                if first:
+                    first = False
+                    continue
                 content.append(row.strip("\r\n"))
             return content
     
     def fillContent(self):
+        """
+        Fills in the content in the database
+        """
         # First delete those tableInfo entries whose corresponding files are not present
         for table in self.tableInfo:
             if os.path.isfile(os.getcwd() + str(table) + ".csv") == False:
@@ -76,37 +86,41 @@ class MiniSQL:
                     d = d.strip('\n')
                     self.database[table][colName].append(int(d))
 
-    def aggregate(self, table, column, fun):
+    def aggregate(self, tableName, column, fun):
         """
-        Gives the aggregate function 'fun' on 'table' for 'column'
+        Gives the aggregate function 'fun' on 'tableName' for 'column'
         """
-        if table not in self.tableInfo.keys():
-            raise FileNotFoundError(str(table) + " table does not exist in the database")
-        if column not in self.tableInfo[table]:
-            raise NotImplementedError(str(table) + " table does not have any column named " + str(column))
+        if tableName not in self.tableInfo.keys():
+            raise FileNotFoundError(str(tableName) + " table does not exist in the database")
+        if column not in self.tableInfo[tableName]:
+            raise NotImplementedError(str(tableName) + " table does not have any column named " + str(column))
 
         if fun == 'MAX':
             val = int(-1e9)
-            for v in self.database[table][column]:
+            for v in self.database[tableName][column]:
                 val = max(val, v)
             return val
         elif fun == 'MIN':
             val = int(1e9)
-            for v in self.database[table][column]:
+            for v in self.database[tableName][column]:
                 val = min(val, v)
             return val
         elif fun == 'COUNT':
-            return len(self.database[table][column])
+            return len(self.database[tableName][column])
         elif fun == 'SUM':
-            return functools.reduce(lambda a,b : a + b, self.database[table][column])
+            return functools.reduce(lambda a,b : a + b, self.database[tableName][column])
         elif fun == 'AVG':
-            summ = functools.reduce(lambda a,b : a + b, self.database[table][column])
-            elements = len(self.database[table][column])
+            summ = functools.reduce(lambda a,b : a + b, self.database[tableName][column])
+            elements = len(self.database[tableName][column])
             return (summ / elements)
         else:
             raise NotImplementedError(str(fun) + " function is not implemented in Mini SQL")
-
-    def condition(self, first, second, operator):
+    
+    @staticmethod
+    def condition(first, second, operator):
+        """
+        checks the condition
+        """
         if operator == '=':
             return first == second
         elif operator == '<':
@@ -120,8 +134,38 @@ class MiniSQL:
         else:
             raise NotImplementedError(str(operator) + " is not implemented in Mini SQL")
     
+    def joinHelper(self, tableList, ind, rowList):
+        """
+        Recursive function for joining tables
+        """
+        if ind == len(tableList):
+            pass
+        table = tableList[ind]
+        colName = self.tableInfo[table][0]
+        for i in range(len(self.database[table][colName])):
+            rowList.append(i)
+            self.joinHelper(tableList, ind+1, rowList)
+            rowList.pop()
+
+    def joinTables(self, tableList):
+        """
+        Cartesian product tables in tableList 
+        """
+        for tableName in tableList:
+            if tableName not in self.tableInfo.keys():
+                raise FileNotFoundError(str(tableName) + " table does not exist in the database")
+        
+        if len(tableList) == 1:
+            return self.database[tableList[0]]
+        table = OrderedDict()
 
 
+    def distinct(self, tableList, columnList):
+        """
+        SELECT DISTINCT col1, col2, .... FROM table1,table2, .... WHERE condition
+        We receive a list of table names, first we need to get a single table by joining them.
+        """
+        pass
 
 
 
